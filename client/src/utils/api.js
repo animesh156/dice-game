b47c086fb57e01ffb783c0850909262fd57cd3dd
animesh <animeshrathore255@gmail.com>
2025-03-08 17:17:24 +0530

    Update frontend with Render backend URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: "http://localhost:6662/api",   // for development purpose
+    // baseURL: "http://localhost:6662/api",   // for development 
+    baseURL: "https://dice-game-backend-pgv6.onrender.com/api",   // for production 
     withCredentials: true
 })
 
