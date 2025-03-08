0bed9f7be92fed8f2c0c3f747559dc0142947e4e
animesh <animeshrathore255@gmail.com>
2025-03-08 17:42:13 +0530

    fixed home page issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,15 @@
 import { useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
+
 import { useNavigate } from "react-router-dom";
 import DiceGame from "../components/DiceGame";
 
-
 function Home() {
   const navigate = useNavigate();
-  
 
   useEffect(() => {
     const isAuthorize = localStorage.getItem("isAuthenticated");
 
     if (!isAuthorize) {
-      toast.error("Only authorized user can access");
-
       setTimeout(() => {
         navigate("/login");
       }, 2000);
